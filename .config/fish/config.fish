eecf95e46bd3b35591d1375290a35c68e648ff8e
Edward McCabe <edm6118@utulsa.edu>
2021-01-17 15:52:40 +0000

    added more dotfiles

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,6 @@
 powerline-daemon -q
 set fish_function_path $fish_function_path "/home/quint/.local/lib/python3.9/site-packages/powerline/bindings/fish"
 powerline-setup
+function fish_mode_prompt
+	# noop
+end
---
+++ .vimrc
@@ -0,0 +1,8 @@
+python3 from powerline.vim import setup as powerline_setup
+python3 powerline_setup()
+python3 del powerline_setup
+
+set ls=2
+set number
+set rnu
+imap jj <Esc>
